963dce2e4233265d1f8bb9c41a534a546dd79a71
Emma-Merlin <110098668+Emma-Merlin@users.noreply.github.com>
2022-08-09 22:26:45 +0100

    Add files via upload

---
+++ Dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta charset="utf-8" name="viewport" content="width=divice-width,initial-scale=1.0"/>
+        <title>e-Diary Management System</title>
+        <link rel="stylesheet" type="text/css" href=".css">
+    </head>
+    <body>
+        <button type="button" onclick="document.getElementById('demo').innerHTML=Date()">click</button>
+        <p id="demo"> good mornimg</p>
+    </body>
+</html>
---
+++ myprofile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta charset="utf-8" name="viewport" content="width=divice-width,initial-scale=1.0"/>
+        <title>e-Diary Management System</title>
+        <link rel="stylesheet" type="text/css" href="setting.css">
+    </head>
+    <style>
+        div{
+            margin: 50px;
+        }
+    </style>
+    <body>
+        <header>
+            <a href="My Project.html"><img src="images/home.png" alt="home" class="home" width="20px" height="20px"></a>
+        </header>
+        <div>
+            <input type="text" name="name" placeholder="Input your name"><br><br>
+            <input type="email" name="old email" placeholder="Input your old Email"><br><br>
+            <input type="email" name="new email" placeholder="Input your new Email"><br><br>
+            <button type="submit">Save</button>
+            <button type="submit">Cancel</button>
+        </div>
+    </body>
+</html>
